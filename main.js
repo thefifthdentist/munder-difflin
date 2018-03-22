b7353ba4ab6121dc53b27451064868c7c078a29d
Mada Romero <madalena73@gmail.com>
2018-03-22 16:23:50 -0400

    Working Page no JS

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-window.onload=toBottom;
+var slideIndex = 1;
+showDivs(slideIndex);
 
-function toBottom() {
-window.scrollTo(0, document.body.scrollHeight);
+function plusDivs(n) {
+  showDivs(slideIndex +=n);
+}
+
+function showDivs(n) {
+  var = i;
+  var x = document.getElementsbyClassName("carousel");
+  if (n > x.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = x.length};
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
