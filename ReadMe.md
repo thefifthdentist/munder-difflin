1ae7693878b24f776bb6ef8a04bdf5fd0d47dd37
Mada Romero <madalena73@gmail.com>
2018-03-26 23:06:57 -0400

    Backing Up

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,1 @@
-This little one-page site is meant to demonstrate basic html, css and javascript skills, as well as taking a tiny spin at design. Enjoy!
+This little one-page site is meant to demonstrate basic html, css and javascript skills, as well as to take a tiny spin at design. Enjoy!
---
+++ main3.js
@@ -0,0 +1,12 @@
+let gameOver = false;
+let computerChoices = ["rock", "paper", "scissors"];
+let computerChoice = randomFrom(computerChoices);
+let playerInput = "";
+
+function randomFrom() {
+  return array[Math.floor(Math.random()*3)];
+function checkInput(input, computerChoices){
+  if(input==="quit")
+  return true;
+}
+}
