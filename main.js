abb91d8bf8cdcee6164b93c7b2de5941029273bd
Mada Romero <madalena73@gmail.com>
2018-03-28 23:38:11 -0400

    Final.2

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-
-
-
-
-//hover
-var e = document.getElementbyId('parent');
-e.onmouseover = function () {
-  document.getElementbyId('popup').style.display = 'block';
-}
-e.onmouseout = function() {
-  document.getElementbyId('popup').style.display = 'none';
-}
-
-
-
-
-//SLIDESHOW
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex +=n);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
-  var = i;
-  var x = document.getElementsbyClassName("carousel");
-  if (n > x.length) {slideIndex = 1};
-  if (n < 1) {slideIndex = x.length};
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
